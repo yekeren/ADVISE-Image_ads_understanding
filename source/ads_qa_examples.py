6befeafdca0be84f23c3fbb9ed1989dc15d62f39
yekeren <yekeren.cn@gmail.com>
2017-09-22 12:18:54 -0400

    Add ads_qa_examples to read from tf record files.

---
+++ source/ads_qa_examples.py
@@ -0,0 +1,128 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+import tensorflow as tf
+
+from tensorflow.contrib.slim.python.slim.data import dataset
+from tensorflow.contrib.slim.python.slim.data import dataset_data_provider
+from tensorflow.contrib.slim.python.slim.data import tfexample_decoder
+
+import ads_emb_model_pb2
+_NUM_EXAMPLES=30000
+
+def _create_tfrecord_dataset(config):
+  """Create tfrecord dataset for DatasetDataProvider.
+
+  Args:
+    config: an instance of AdsQAExample proto.
+
+  Returns:
+    dataset: a slim.data.dataset.Dataset instance.
+  """
+  keys_to_features = {
+    'image/source_id': tf.FixedLenFeature(
+        shape=(), dtype=tf.string, default_value=''),
+    'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
+    'image/format': tf.FixedLenFeature((), tf.string, default_value='jpeg'),
+    'caption/num_captions': tf.FixedLenFeature(shape=(), dtype=tf.int64),
+    'caption/caption_lengths': tf.FixedLenFeature(
+        shape=[config.max_num_captions], dtype=tf.int64),
+    'caption/caption_strings': tf.FixedLenFeature(
+        shape=[config.max_num_captions, config.max_caption_len], dtype=tf.int64)
+  }
+  items_to_handlers = {
+    'image_id': tfexample_decoder.Tensor('image/source_id'),
+    'image': tfexample_decoder.Image(
+        shape=[config.image_height, config.image_width, 3]),
+    'num_captions': tfexample_decoder.Tensor('caption/num_captions'),
+    'caption_lengths': tfexample_decoder.Tensor('caption/caption_lengths'),
+    'caption_strings': tfexample_decoder.Tensor('caption/caption_strings'),
+  }
+
+  if config.export_feature:
+    if not config.image_level_feature:
+      # Treat each entities in an image individually.
+      keys_to_features['entity/num_entities'] = tf.FixedLenFeature(
+          shape=(), dtype=tf.int64)
+      keys_to_features['entity/embeddings'] = tf.FixedLenFeature(
+          shape=[config.max_detections, config.feature_dimentions],
+          dtype=tf.float32)
+      items_to_handlers['num_entities'] = tfexample_decoder.Tensor(
+          'entity/num_entities')
+      items_to_handlers['embeddings'] = tfexample_decoder.Tensor(
+          'entity/embeddings')
+
+    else:
+      # Treat the whole image as an single entity.
+      keys_to_features['entity/fake_one'] = tf.FixedLenFeature(
+          shape=(), dtype=tf.int64, default_value=1)
+      keys_to_features['image/embeddings'] =  tf.FixedLenFeature(
+          shape=[1, config.feature_dimentions], dtype=tf.float32)
+      items_to_handlers['num_entities'] = tfexample_decoder.Tensor(
+          'entity/fake_one')
+      items_to_handlers['embeddings'] = tfexample_decoder.Tensor(
+          'image/embeddings')
+
+  decoder = tfexample_decoder.TFExampleDecoder(
+      keys_to_features, items_to_handlers)
+
+  return dataset.Dataset(
+      data_sources=config.input_path,
+      reader=tf.TFRecordReader,
+      decoder=decoder,
+      num_samples=config.num_examples,
+      items_to_descriptions=None)
+
+
+def get_examples(config):
+  """Get batched tensor of training data.
+
+  Args:
+    config: an instance of AdsQAExample proto.
+
+  Returns:
+    tensor_dict: a dictionary mapping data names to tensors.
+      'image_id':          tf.string,  [batch]
+      'image':             tf.uint8    [batch, height, width, 3]
+      'num_captions':      tf.int64,   [batch]
+      'caption_lengths':   tf.int64,   [batch, max_num_captions]
+      'caption_strings':   tf.int64,   [batch, max_num_captions, max_caption_len]
+      'num_detections':    tf.int64,   [batch]
+      'proposed_features': tf.float32, [batch, max_detections, feature_dimentions]
+
+  Raises:
+    ValueError: if config is invalid.
+  """
+  if not isinstance(config, ads_emb_model_pb2.AdsQAExamples):
+    raise ValueError('config has to be an instance of AdsQAExamples.')
+
+  dataset = _create_tfrecord_dataset(config)
+  provider = dataset_data_provider.DatasetDataProvider(
+      dataset,
+      shuffle=True,
+      num_readers=config.data_provider_num_readers,
+      common_queue_capacity=config.data_provider_common_queue_capacity,
+      common_queue_min=config.data_provider_common_queue_min)
+
+  # TODO(yek@): why record_key is there?
+  items = provider.list_items()
+  items = filter(lambda x: x != 'record_key', items)
+
+  data = provider.get(items)
+  data = tf.train.batch(data,
+      config.batch_size, 
+      enqueue_many=False, 
+      num_threads=config.batch_op_num_threads, 
+      capacity=config.batch_op_capacity)
+
+  name_dict = {
+    'num_entities': 'num_detections',
+    'embeddings': 'proposed_features',
+  }
+  tensor_dict = {}
+  for item, tensor in zip(items, data):
+    tensor_dict[name_dict.get(item, item)] = tensor
+  return tensor_dict
---
+++ source/ads_qa_examples_test.py
@@ -0,0 +1,186 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import numpy as np
+import tensorflow as tf
+
+from google.protobuf import text_format
+
+import ads_emb_model_pb2
+import ads_qa_examples
+
+slim = tf.contrib.slim
+
+
+class AdsQAExamplesTest(tf.test.TestCase):
+  def setUp(self):
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    config_str = """
+      input_path: "output/ads_qa.mobilenet_v1.train.record" 
+      batch_size: 32
+      image_height: 500
+      image_width: 500
+      max_num_captions: 5
+      max_caption_len: 30
+      export_feature: true
+      max_detections: 10
+      feature_dimentions: 1024
+      image_level_feature: true
+    """
+    self.default_mobilenet_v1_config = ads_emb_model_pb2.AdsQAExamples()
+    text_format.Merge(config_str, self.default_mobilenet_v1_config)
+
+    config_str = """
+      input_path: "output/ads_qa.inception_v4.train.record" 
+      batch_size: 32
+      image_height: 500
+      image_width: 500
+      max_num_captions: 5
+      max_caption_len: 30
+      export_feature: true
+      max_detections: 10
+      feature_dimentions: 1536
+      image_level_feature: true
+    """
+    self.default_inception_v4_config = ads_emb_model_pb2.AdsQAExamples()
+    text_format.Merge(config_str, self.default_inception_v4_config)
+
+  def test_mobilenet_v1_feature(self):
+    # Image level feature.
+    config = self.default_mobilenet_v1_config
+    config.image_level_feature = True
+
+    g = tf.Graph()
+    with g.as_default():
+      example = ads_qa_examples.get_examples(config)
+
+    with self.test_session(graph=g) as sess:
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      example = sess.run(example)
+      self.assertEqual(example['image'].shape, (32, 500, 500, 3))
+      self.assertEqual(example['num_captions'].shape, (32,))
+      self.assertEqual(example['caption_lengths'].shape, (32, 5))
+      self.assertEqual(example['caption_strings'].shape, (32, 5, 30))
+      self.assertEqual(example['num_detections'].shape, (32,))
+      self.assertEqual(example['proposed_features'].shape, (32, 1, 1024))
+
+      coord.request_stop()
+      coord.join(threads, stop_grace_period_secs=10)
+
+    # Patch level feature.
+    config = self.default_mobilenet_v1_config
+    config.image_level_feature = False
+
+    g = tf.Graph()
+    with g.as_default():
+      example = ads_qa_examples.get_examples(config)
+
+    with self.test_session(graph=g) as sess:
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      example = sess.run(example)
+      self.assertEqual(example['image'].shape, (32, 500, 500, 3))
+      self.assertEqual(example['num_captions'].shape, (32,))
+      self.assertEqual(example['caption_lengths'].shape, (32, 5))
+      self.assertEqual(example['caption_strings'].shape, (32, 5, 30))
+      self.assertEqual(example['num_detections'].shape, (32,))
+      self.assertEqual(example['proposed_features'].shape, (32, 10, 1024))
+
+      coord.request_stop()
+      coord.join(threads, stop_grace_period_secs=10)
+
+    # Do not export feature.
+    config = self.default_mobilenet_v1_config
+    config.export_feature = False
+
+    g = tf.Graph()
+    with g.as_default():
+      example = ads_qa_examples.get_examples(config)
+
+    with self.test_session(graph=g) as sess:
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      example = sess.run(example)
+      self.assertEqual(example['image'].shape, (32, 500, 500, 3))
+      self.assertEqual(example['num_captions'].shape, (32,))
+      self.assertEqual(example['caption_lengths'].shape, (32, 5))
+      self.assertEqual(example['caption_strings'].shape, (32, 5, 30))
+      self.assertNotIn('num_detections', example)
+      self.assertNotIn('proposed_features', example)
+
+      coord.request_stop()
+      coord.join(threads, stop_grace_period_secs=10)
+
+  def test_inception_v4_feature(self):
+    # Image level feature.
+    config = self.default_inception_v4_config
+    config.image_level_feature = True
+
+    g = tf.Graph()
+    with g.as_default():
+      example = ads_qa_examples.get_examples(config)
+
+    with self.test_session(graph=g) as sess:
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      example = sess.run(example)
+      self.assertEqual(example['image'].shape, (32, 500, 500, 3))
+      self.assertEqual(example['num_captions'].shape, (32,))
+      self.assertEqual(example['caption_lengths'].shape, (32, 5))
+      self.assertEqual(example['caption_strings'].shape, (32, 5, 30))
+      self.assertEqual(example['num_detections'].shape, (32,))
+      self.assertEqual(example['proposed_features'].shape, (32, 1, 1536))
+
+      coord.request_stop()
+      coord.join(threads, stop_grace_period_secs=10)
+
+    # Patch level feature.
+    config = self.default_inception_v4_config
+    config.image_level_feature = False
+
+    g = tf.Graph()
+    with g.as_default():
+      example = ads_qa_examples.get_examples(config)
+
+    with self.test_session(graph=g) as sess:
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      example = sess.run(example)
+      self.assertEqual(example['image'].shape, (32, 500, 500, 3))
+      self.assertEqual(example['num_captions'].shape, (32,))
+      self.assertEqual(example['caption_lengths'].shape, (32, 5))
+      self.assertEqual(example['caption_strings'].shape, (32, 5, 30))
+      self.assertEqual(example['num_detections'].shape, (32,))
+      self.assertEqual(example['proposed_features'].shape, (32, 10, 1536))
+
+      coord.request_stop()
+      coord.join(threads, stop_grace_period_secs=10)
+
+    # Do not export feature.
+    config = self.default_inception_v4_config
+    config.export_feature = False
+
+    g = tf.Graph()
+    with g.as_default():
+      example = ads_qa_examples.get_examples(config)
+
+    with self.test_session(graph=g) as sess:
+      coord = tf.train.Coordinator()
+      threads = tf.train.start_queue_runners(coord=coord)
+      example = sess.run(example)
+      self.assertEqual(example['image'].shape, (32, 500, 500, 3))
+      self.assertEqual(example['num_captions'].shape, (32,))
+      self.assertEqual(example['caption_lengths'].shape, (32, 5))
+      self.assertEqual(example['caption_strings'].shape, (32, 5, 30))
+      self.assertNotIn('num_detections', example)
+      self.assertNotIn('proposed_features', example)
+
+      coord.request_stop()
+      coord.join(threads, stop_grace_period_secs=10)
+if __name__ == '__main__':
+    tf.test.main()
